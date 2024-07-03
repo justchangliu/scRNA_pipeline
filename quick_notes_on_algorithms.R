bff6c0b165f1453709ce3e9ed0c805439780c261
Chang Liu <cl4493@cumc.columbia.edu>
2024-07-03 18:41:32 -0400

    Update quick_notes_on_algorithms.R

--- quick_notes_on_algorithms.R
+++ quick_notes_on_algorithms.R
@@ -1,1 +1,14 @@
+#### Normalize method ####
+# LogNormalize calculation: log2((count/total_counts)*10000+1)
+# just a regular method to avoid the depth of the library to impact the variances from barcode to barcode
+# This is A MUST DO otherwise there is not much sense to work with the data at all.
+# correct barcode direction technical errors.
+
+#### Find Variable Features ####
+# just try to find a pseudo-linear correlation between mean(expression) and variance
+# and get the standard deviation 
+# different non-supervised method to estimate the method to pick up features
+
+
+#### ScaleData ####
 
